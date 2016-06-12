8a071448468921f21211f89c653d501df2d6617f
Bill Stiber <billstiber@gmail.com>
2016-06-11 20:32:55 -0400

    add file loops.rb

---
+++ branching_and_looping/loops.rb
@@ -0,0 +1,9 @@
+# loops3.rb more practice looping
+
+friends = ['sally', 'jane', 'bob', 'joseph']
+
+friends.each do |friend|
+  friend.concat( " loves good food, movies and popkorn")
+end
+
+puts friends
