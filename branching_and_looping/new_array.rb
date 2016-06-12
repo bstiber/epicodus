1f620a5329bd953bc32b413eea40fafccae5d48a
Bill Stiber <billstiber@gmail.com>
2016-06-11 20:54:26 -0400

    add exercises

--- branching_and_looping/new_array.rb
+++ branching_and_looping/new_array.rb
@@ -5,3 +5,13 @@ names.each do |name|
   new_names.push(name.upcase)
 end
 puts new_names
+
+# numbers looping practice
+
+numbers = [1, 2, 3, 4, 5, 6, 8, 9, 10]
+new_numbers = []
+
+numbers.each do |number|
+  new_numbers.push(number + 2)
+end
+puts new_numbers
