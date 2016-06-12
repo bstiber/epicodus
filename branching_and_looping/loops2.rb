c9633ae337192646bbeb11a5869b3f42ef2a1f1e
Bill Stiber <billstiber@gmail.com>
2016-06-11 20:07:10 -0400

    add file loops2 and exercises on basic loops and practice

---
+++ branching_and_looping/loops2.rb
@@ -0,0 +1,13 @@
+# loops2.rb
+#
+# removing the last 4 items of an array using pop
+
+array = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9]
+array.pop(4)
+
+# adding a number plus itself over and over
+number = 0
+5.times do
+  number = number + 5
+end
+number
